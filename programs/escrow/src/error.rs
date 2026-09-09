0531b7694f7a75cfebfb986907639bfbeccba816
SolarDePinHub <team.solardepinhub@gmail.com>
2026-09-09 01:19:47 +0000

    Add five minute time lock before escrow cancellation

--- programs/escrow/src/error.rs
+++ programs/escrow/src/error.rs
@@ -2,6 +2,8 @@ use anchor_lang::prelude::*;
 
 #[error_code]
 pub enum ErrorCode {
+    #[msg("The time lock has not elapsed")]
+    TimeLockActive,
     #[msg("Custom error message")]
     CustomError,
 }
