3800e7223cc66dd048ce8807501980d309b0057f
Xinyan Liu <liuxinyan0112@foxmail.com>
2026-09-09 20:17:47 +0200

    Add five minute time lock before escrow cancellation

--- programs/escrow/src/error.rs
+++ programs/escrow/src/error.rs
@@ -4,4 +4,6 @@ use anchor_lang::prelude::*;
 pub enum ErrorCode {
     #[msg("Custom error message")]
     CustomError,
+    #[msg("The offer cannot be cancelled until five minutes after it was created")]
+    TimeLockActive,
 }
